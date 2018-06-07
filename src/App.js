4bbc2f1fd7cccb16d20fa7bc1e647210fcc3b0e8
a <anshgujral1997@gmail.com>
2018-06-07 05:33:41 -0400

    initiated firebase

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import {View, Text} from 'react-native';
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
 import reducers from './reducers';
+import firebase from './firebase/firebase';
 
 class App extends Component {
+  
   render() {
     return (
       <Provider store={createStore(reducers)}>
