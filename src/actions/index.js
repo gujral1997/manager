22abe860939ce06cc776d9aa4b627a754fa6f25c
a <anshgujral1997@gmail.com>
2018-06-10 18:32:58 -0400

    stored login user failed state in the store

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,9 @@
-import {EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS} from './types'
+import {
+    EMAIL_CHANGED,
+    PASSWORD_CHANGED,
+    LOGIN_USER_SUCCESS,
+    LOGIN_USER_FAIL
+} from './types'
 import firebase from 'firebase'
 
 export const emailChanged = (text) => {
@@ -18,8 +23,22 @@ export const passwordChanged = (text) => {
 export const loginUser = ({email, password}) => {
     return (dispatch) => {
         firebase.auth().signInWithEmailAndPassword(email, password)
-            .then(user =>{
-                dispatch({type: LOGIN_USER_SUCCESS, payload: user})
+            .then(user =>loginUserSuccess(dispatch, user))
+            .catch(()=>{
+                firebase.auth().createUserAndRetrieveDataWithEmailAndPassword(email, password)
+                .then(user=>loginUserSuccess(dispatch, user))
+                .catch(()=> loginUserFail(dispatch))
             })
     }
+}
+
+const loginUserSuccess = (dispatch, user) => {
+    dispatch({
+        type: LOGIN_USER_SUCCESS,
+        payload: user
+    })
+}
+
+const loginUserFail = (dispatch, user) => {
+    dispatch({type: LOGIN_USER_FAIL})
 }
--- src/actions/types.js
+++ src/actions/types.js
@@ -1,3 +1,4 @@
 export const EMAIL_CHANGED = 'email_changed'
 export const PASSWORD_CHANGED = 'password_chaneged'
 export const LOGIN_USER_SUCCESS = 'LOGIN_USER_SUCCESS'
+export const LOGIN_USER_FAIL = 'LOGIN_USER_FAIL'
